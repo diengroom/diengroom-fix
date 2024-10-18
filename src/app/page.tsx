d3a2819bdde0688c69ea5d73330c1c56e13928ff
diengroom <185160871+diengroom@users.noreply.github.com>
2024-10-18 17:30:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'; // Custom header component
+import HomePage from './pages';
+import Home from './pages';
+
+const Page = () => {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+};
+
+export default Page;
